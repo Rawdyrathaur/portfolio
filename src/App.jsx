dcc18ab5e8acf80531d01c14f3753d2c2cfe2acc
Rawdyrathaur <mrathaur704@gmail.com>
2026-04-18 00:09:45 +0530

    updated new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/index.css'
 import Hero from './components/Hero/Hero'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <main>
       <Hero />
+      <Navbar />
     </main>
   )
 }
