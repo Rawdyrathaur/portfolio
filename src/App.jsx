8ec5a49b059abacc85c1e8b23f416848b95c46ca
Rawdyrathaur <mrathaur704@gmail.com>
2026-04-18 23:04:59 +0530

    add new feature.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import ThemeToggle from './components/ThemeToggle/ThemeToggle'
 
+
 import ReadMode from './components/ReadMode/ReadMode'
 function App() {
   return (
