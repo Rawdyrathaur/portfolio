b210492366b88d765c07e19299c3739be85d31d9
Rawdyrathaur <mrathaur704@gmail.com>
2026-05-27 01:29:47 +0530

    fix

--- src/components/UnderConstruction/UnderConstruction.jsx
+++ src/components/UnderConstruction/UnderConstruction.jsx
@@ -3,11 +3,7 @@ import './UnderConstruction.css'
 export default function UnderConstruction() {
   return (
     <div className="under-construction">
-      <img 
-        src="https://media.giphy.com/media/l0HlDy9x8FZo0XO1i/giphy.gif" 
-        alt="Under Construction" 
-        className="construction-sticker"
-      />
+    
       <p>🚧 Under Construction 🚧</p>
     </div>
   )
